d73341e96d66148f5682284fad5ac247c9fbb380
Stefan Stefanov <stefan.bg@gmail.com>
2017-12-21 15:22:42 +0200

    Initial commit

---
+++ src/TimeDeltaFormatter.php
@@ -0,0 +1,262 @@
+<?php
+
+namespace stv;
+
+class TimeDeltaFormatter
+{
+    /**
+     * Base time for calculating deltas
+     *
+     * @var int Unix timestamp
+     */
+    public static $now;
+
+    /**
+     * Time to convert, relative to self::$now
+     *
+     * @var int Unix timestamp
+     */
+    protected $time;
+
+    /**
+     * @var UtilitiesInterface
+     */
+    public static $utilities;
+
+    /**
+     * Internal structure holding time delta parts.
+     *
+     * Time parts are the same returned by PHP function @see getdate()
+     *
+     * @var array
+     */
+    protected $deltaParts = array();
+
+    const A_DAY_IN_SECONDS = 86399; // = (24h in seconds) minus one second
+
+    const DIR = [
+        'PAST' => false,
+        'FUTURE' => true
+    ];
+
+    public function __construct($time)
+    {
+        $this->setTime($time);
+    }
+
+    public function round()
+    {
+        $r = $this->deltaParts;
+
+        // Round minutes
+        switch (true) {
+            case ($r['s'] > 50):
+                $r['i']++;
+                break;
+            case ($r['i'] >= 26 && $r['i'] <= 34):
+                // 26 - 34 min
+                $r['i'] = 30;
+                break;
+            case ($r['i'] >= 55):
+                // 55 - 59 min
+                $r['i'] = 0;
+                $r['H']++;
+                break;
+        }
+
+        if ($r['H'] >= 1 && $r['i'] <= 5) {
+            $r['i'] = 0;
+        }
+
+        // Round hours
+        if (!$this->isToday() && !$this->isYesterday() && !$this->isTomorrow()) {
+            $r['d'] = max($r['d'], 2);
+            $r['i'] =
+            $r['H'] = 0;
+        }
+
+        // Round days
+        if ($r['d'] > 25) {
+            $r['d'] =
+            $r['i'] =
+            $r['H'] = 0;
+            $r['m']++;
+        }
+
+        // Round months
+        if ($r['m'] >= 12) {
+            $r['m'] %= 12;
+            if ($r['m'] >= 5 || $r['m'] <= 7) {
+                $r['m'] = 6;
+            }
+            $r['d'] =
+            $r['i'] =
+            $r['H'] = 0;
+            $r['Y']++;
+        }
+
+        if ($r['Y'] > 1 && $r['m'] < 2) {
+            $r['m'] = 0;
+        }
+
+        // Always ignore seconds
+        $r['s'] = 0;
+
+        return $r;
+    }
+
+    public function say($r)
+    {
+        $inWords = '';
+        $prependDir = true;
+
+        switch (true) {
+            case $r['Y'] == 1 && $r['m'] == 0:
+                $inWords = self::$utilities::__('година');
+                break;
+            case $r['Y'] == 1:
+                $inWords = self::$utilities::__('година и %d мес.', $r['m']);
+                break;
+            case $r['Y'] > 1 && $r['m'] == 0:
+                $inWords = self::$utilities::__('години', $r['Y']);
+                break;
+            case $r['Y'] > 1:
+                $inWords = self::$utilities::__('%d години и %d мес.', $r['Y'], $r['m']);
+                break;
+
+            case $r['m'] == 1 && $r['d'] == 0:
+                $inWords = self::$utilities::__('месец');
+                break;
+            case $r['m'] == 1 :
+                $inWords = self::$utilities::__('месец и %d дни', $r['d']);
+                break;
+            case $r['m'] > 1 && $r['d'] == 0:
+                $inWords = self::$utilities::__('%d месеца', $r['m']);
+                break;
+            case $r['m'] > 1 && $r['d'] < 5:
+                $inWords = self::$utilities::__('близо %d месеца', $r['m']);
+                break;
+            case $r['m'] > 1 && $r['d'] > 25 :
+                $inWords = self::$utilities::__('близо %d месеца', $r['m']+1);
+                break;
+            case $r['m'] > 4:
+                $inWords = self::$utilities::__('%d месеца', $r['m']);
+                break;
+            case $r['m'] > 1:
+                $inWords = self::$utilities::__('%d месеца и %d дни', $r['m'], $r['d']);
+                break;
+
+            case $r['d'] > 1:
+                $inWords = self::$utilities::__('%d дни', $r['d']);
+                break;
+
+            case $this->deltaHr <= 6 && $r['H'] >= 1:
+                if ($r['H'] == 1) {
+                    $inWords = self::$utilities::__('час');
+                } else {
+                    $inWords = self::$utilities::__('%d часа', $r['H']);
+                }
+                if ($r['i'] > 1) {
+                    $inWords .= ' ' . self::$utilities::__('и %d мин.', $r['i']);
+                }
+                break;
+
+            case $this->deltaHr > 6:
+                assert ($r['d'] <= 1);
+                $time = date('H:i', $this->time);
+                if ($this->isToday()) {
+                    $inWords = self::$utilities::__('днес');
+                } elseif ($this->isTomorrow()) {
+                    $inWords = self::$utilities::__('утре');
+                } elseif ($this->isYesterday()) {
+                    $inWords = self::$utilities::__('вчера');
+                }
+
+                $inWords .= ' ' . self::$utilities::__('в %s ч.', $time);
+                $prependDir = false;
+                break;
+
+
+            case $r['i'] > 1:
+                $inWords = self::$utilities::__('%d минути', $r['i']);
+                break;
+            case $r['i'] == 1:
+                $inWords = self::$utilities::__('минута');
+                break;
+
+            default:
+                $inWords = self::$utilities::__('сега');
+                $prependDir = false;
+        }
+
+        if ($prependDir) {
+            switch ($this->dir) {
+                case self::DIR['FUTURE']:
+                    $dirText = 'след';
+                    break;
+                case self::DIR['PAST']:
+                    $dirText = 'преди';
+                    break;
+            }
+
+            $inWords = $dirText . ' ' . $inWords;
+        }
+
+        return $inWords;
+    }
+
+    protected function isToday()
+    {
+        return date('Y-m-d', self::$now) == date('Y-m-d', $this->time);
+    }
+
+    protected function isTomorrow()
+    {
+        return date('Y-m-d', self::$now + self::A_DAY_IN_SECONDS) == date('Y-m-d', $this->time);
+    }
+
+    protected function isYesterday()
+    {
+        return date('Y-m-d', self::$now - self::A_DAY_IN_SECONDS) == date('Y-m-d', $this->time);
+    }
+
+    public function setTime($time)
+    {
+        if (!is_numeric($time)) {
+            $time = strtotime($time);
+        }
+
+        $this->time = $time;
+
+        // re-calculate after time change
+        if ($this->time < self::$now) {
+            $this->dir = self::DIR['PAST'];
+        } else {
+            $this->dir = self::DIR['FUTURE'];
+        }
+
+        $delta = abs($this->time - self::$now);
+        $this->deltaParts = array(
+            's' => $delta % 60,
+            'i' => intval($delta / 60) % 60,
+            'H' => intval($delta / (60 * 60)) % 24,
+            'd' => intval($delta / (60 * 60 * 24)) % 30,
+            'm' => intval($delta / (60 * 60 * 24 * 30)) % 12,
+            'Y' => intval($delta / (60 * 60 * 24 * 30 * 12)),
+        );
+
+        $this->deltaSec = abs($this->time - self::$now);
+        $this->deltaMin = $this->deltaSec / 60;
+        $this->deltaHr  = $this->deltaMin / 60;
+        $this->deltaDay = $this->deltaHr / 24;
+    }
+
+    public static function convert($time)
+    {
+        $self = new self($time);
+
+        return $self->say($self->round());
+    }
+}
+
+TimeDeltaFormatter::$now = time();
---
+++ test/TimeDeltaFormatterTest.php
@@ -0,0 +1,158 @@
+<?php
+
+use stv\TimeDeltaFormatter;
+use stv\UtilitiesInterface;
+use PHPUnit\Framework\TestCase;
+
+class Utilities implements UtilitiesInterface {
+    public static function __(string $str, ...$args): string {
+        return sprintf($str, ...$args);
+    }
+}
+
+/**
+ * TimeDeltaFormatter Test Case
+ *
+ */
+class TimeDeltaFormatterTest extends TestCase
+{
+
+    protected function setUp()
+    {
+        TimeDeltaFormatter::$now = strtotime('2012-11-08 12:02:00');
+        TimeDeltaFormatter::$utilities = Utilities::class;
+    }
+
+    protected static function s($s)
+    {
+        return TimeDeltaFormatter::$now + $s;
+    }
+
+    protected static function m($m, $s = 0)
+    {
+        return self::s($m * 60 + $s);
+    }
+
+    protected static function h($h, $m = 0, $s = 0)
+    {
+        return self::m($h * 60 + $m, $s);
+    }
+
+    protected static function d($d, $h = 0, $m = 0)
+    {
+        return self::h($d * 24 + $h, $m);
+    }
+
+    public function testNow()
+    {
+        // преди или след по-малко от една минута
+        $this->assertEquals('сега', TimeDeltaFormatter::convert(self::s(50)));
+        $this->assertEquals('сега', TimeDeltaFormatter::convert(self::s(-50)));
+    }
+
+    public function testMinutes()
+    {
+        $this->assertEquals('след минута', TimeDeltaFormatter::convert(self::s(51)));
+        $this->assertEquals('преди минута', TimeDeltaFormatter::convert(self::s(-51)));
+        $this->assertEquals('след минута', TimeDeltaFormatter::convert(self::m(1, 29)));
+        $this->assertEquals('преди минута', TimeDeltaFormatter::convert(self::m(-1, -29)));
+        $this->assertEquals('след минута', TimeDeltaFormatter::convert(self::m(1, 50)));
+        $this->assertEquals('преди минута', TimeDeltaFormatter::convert(self::m(-1, -50)));
+        $this->assertEquals('след 30 минути', TimeDeltaFormatter::convert(self::m(26)));
+        $this->assertEquals('преди 30 минути', TimeDeltaFormatter::convert(self::m(-26)));
+        $this->assertEquals('след 35 минути', TimeDeltaFormatter::convert(self::m(35)));
+        $this->assertEquals('след 50 минути', TimeDeltaFormatter::convert(self::m(50)));
+        $this->assertEquals('преди 50 минути', TimeDeltaFormatter::convert(self::m(-50)));
+    }
+
+    public function testHoursToday()
+    {
+        $this->assertEquals('след 30 минути', TimeDeltaFormatter::convert(self::m(26)));
+        $this->assertEquals('след 30 минути', TimeDeltaFormatter::convert(self::m(34)));
+        $this->assertEquals('преди 30 минути', TimeDeltaFormatter::convert(self::m(-26)));
+        $this->assertEquals('преди 30 минути', TimeDeltaFormatter::convert(self::m(-34)));
+
+        // До +/- 1 час и 10 мин се закръгля на 1 час
+        $this->assertEquals('след час', TimeDeltaFormatter::convert(self::m(55, 1)));
+        $this->assertEquals('преди час', TimeDeltaFormatter::convert(self::m(-55, -1)));
+        $this->assertEquals('преди час', TimeDeltaFormatter::convert(self::h(-1, -5, 1)));
+
+        $this->assertEquals('след час и 15 мин.', TimeDeltaFormatter::convert(self::h(1, 15)));
+        $this->assertEquals('преди час и 15 мин.', TimeDeltaFormatter::convert(self::h(-1, -15)));
+        $this->assertEquals('след час и 30 мин.', TimeDeltaFormatter::convert(self::h(1, 26)));
+        $this->assertEquals('след час и 30 мин.', TimeDeltaFormatter::convert(self::h(1, 34)));
+        $this->assertEquals('преди час и 30 мин.', TimeDeltaFormatter::convert(self::h(-1, -26)));
+        $this->assertEquals('преди час и 30 мин.', TimeDeltaFormatter::convert(self::h(-1, -34)));
+        $this->assertEquals('след час и 35 мин.', TimeDeltaFormatter::convert(self::h(1, 35)));
+        $this->assertEquals('преди час и 35 мин.', TimeDeltaFormatter::convert(self::h(-1, -35)));
+
+        $this->assertEquals('след 2 часа', TimeDeltaFormatter::convert(self::h(1, 55)));
+        $this->assertEquals('преди 2 часа', TimeDeltaFormatter::convert(self::h(-1, -55)));
+        $this->assertEquals('след 2 часа и 15 мин.', TimeDeltaFormatter::convert(self::h(2, 15)));
+        $this->assertEquals('преди 2 часа и 15 мин.', TimeDeltaFormatter::convert(self::h(-2, -15)));
+        $this->assertEquals('след 2 часа и 30 мин.', TimeDeltaFormatter::convert(self::h(2, 26)));
+        $this->assertEquals('след 2 часа и 30 мин.', TimeDeltaFormatter::convert(self::h(2, 34)));
+        $this->assertEquals('преди 2 часа и 30 мин.', TimeDeltaFormatter::convert(self::h(-2, -26)));
+        $this->assertEquals('преди 2 часа и 30 мин.', TimeDeltaFormatter::convert(self::h(-2, -34)));
+        $this->assertEquals('след 2 часа и 35 мин.', TimeDeltaFormatter::convert(self::h(2, 35)));
+        $this->assertEquals('преди 2 часа и 35 мин.', TimeDeltaFormatter::convert(self::h(-2, -35)));
+
+        // Бъдещ момент от същия ден на повече от 8 часа
+        $this->assertEquals('днес в 23:02 ч.', TimeDeltaFormatter::convert(self::h(11)));
+
+        // Минал момент от същия ден
+        $this->assertEquals('днес в 01:02 ч.', TimeDeltaFormatter::convert(self::h(-11)));
+    }
+
+    public function testHours()
+    {
+        TimeDeltaFormatter::$now = strtotime('2012-11-08 20:00');
+
+        // Бъдещ момент на другия ден, но след по-малко от 6 часа
+        $this->assertEquals('след 6 часа', TimeDeltaFormatter::convert(self::h(6)));
+
+        // Бъдещ момент на другия ден, но след повече от 6 часа
+        $this->assertEquals('утре в 06:20 ч.', TimeDeltaFormatter::convert(self::h(10, 20)));
+
+        TimeDeltaFormatter::$now = strtotime('2012-11-08 4:00');
+
+        // Минал момент на предишния ден, но преди по-малко от 6 часа
+        $this->assertEquals('преди 6 часа', TimeDeltaFormatter::convert(self::h(-6)));
+
+        // Минал момент на предишния ден, но преди повече от 6 часа
+        $this->assertEquals('вчера в 18:00 ч.', TimeDeltaFormatter::convert(self::h(-10)));
+
+    }
+
+    public function testThisMonth()
+    {
+        $this->assertEquals('след 2 дни', TimeDeltaFormatter::convert(self::d(2)));
+        $this->assertEquals('преди 2 дни', TimeDeltaFormatter::convert(self::d(-2)));
+
+    }
+
+    public function testDaysDelta()
+    {
+        TimeDeltaFormatter::$now = strtotime('2012-11-08 00:00');
+
+        $this->assertEquals('след 5 дни', TimeDeltaFormatter::convert('2012-11-13 16:00'));
+        $this->assertEquals('след 5 дни', TimeDeltaFormatter::convert('2012-11-13 23:00'));
+    }
+
+    /**
+     * Когато делтата е под 1 година, над 1 месец и близо до точен брой месеци (+-5 дни)
+     */
+    public function testNearlyMonths()
+    {
+        TimeDeltaFormatter::$now = strtotime('2013-01-17 12:01');
+
+        $this->assertEquals('преди близо 2 месеца', TimeDeltaFormatter::convert('2012-11-17 00:00'));
+    }
+
+    public function testYesterday()
+    {
+        TimeDeltaFormatter::$now = strtotime('2013-02-17 16:26:00');
+
+        $this->assertEquals('вчера в 14:00 ч.', TimeDeltaFormatter::convert('2013-02-16 14:00:00'));
+    }
+}
